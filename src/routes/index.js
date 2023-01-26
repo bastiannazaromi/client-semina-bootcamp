0168fd04c2a23339dcee5dc45d2a13ef855f17c7
Bastian Nazaromi <bastian.nazaromi@gmail.com>
2023-01-26 08:25:29 +0700

    feat: orders

--- src/routes/index.js
+++ src/routes/index.js
@@ -8,8 +8,8 @@ import { CategoriesRoute } from "./CategoriesRoute";
 import { TalentsRoute } from "./TalentsRoute";
 import { PaymentsRoute } from "./PaymentsRoute";
 import SNavbar from "../components/Navbar";
-// import { EventsRoute } from "./EventsRoute";
-// import { OrdersRoute } from "./OrdersRoute";
+import { EventsRoute } from "./EventsRoute";
+import { OrdersRoute } from "./OrdersRoute";
 
 export function AppRoutes() {
     return (
@@ -35,8 +35,8 @@ export function AppRoutes() {
                 <Route path="categories/*" element={<CategoriesRoute />} />
                 <Route path="talents/*" element={<TalentsRoute />} />
                 <Route path="payments/*" element={<PaymentsRoute />} />
-                {/* <Route path="events/*" element={<EventsRoute />} /> */}
-                {/* <Route path="orders/*" element={<OrdersRoute />} /> */}
+                <Route path="events/*" element={<EventsRoute />} />
+                <Route path="orders/*" element={<OrdersRoute />} />
                 <Route
                     path=""
                     element={<Navigate to="/dashboard" replace={true} />}
